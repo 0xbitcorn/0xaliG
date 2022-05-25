9b9728725c9d41cee8ae530a3f1f9f582e8aa136
sk33z3r <sk33z3r@protonmail.com>
2022-05-25 13:28:51 -0400

    fix: run command, add restart

--- docker.sh
+++ docker.sh
@@ -4,15 +4,20 @@ name="0xalig"
 auth="$PWD/auth.json"
 
 run() {
-    docker run -d --init --cap-add=SYS_ADMIN --name $name -v $auth:/usr/src/app/auth.json $name node -e "`cat bot.js`"
+    case $1 in
+        temp) opt="-it --rm";;
+        *) opt="-d";;
+    esac
+    docker run $opt --init --cap-add=SYS_ADMIN --restart unless-stopped --name $name -v $auth:/usr/src/app/auth.json $name node -e "`cat bot.js`"
 }
 
 case $1 in
     build) docker build -t $name .;;
     logs) docker logs -tf $name;;
     restart) docker restart $name;;
-    down) docker stop $name >/dev/null && docker rm $name >/dev/null;;
-    local) docker run -it --rm --name $name -v $auth:/usr/src/app/auth.json $name;;
+    stop) docker stop $name;;
+    kill) docker docker rm -f $name >/dev/null;;
+    local) run temp;;
     status) docker ps -a | grep $name;;
     update)
         git pull &&
