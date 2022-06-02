928d2a623d4a809b8cf86e177308dc0ce1b47277
BTCornBLAIQchnz <104954599+BTCornBLAIQchnz@users.noreply.github.com>
2022-06-02 09:59:02 -0600

    queue fix

--- bot.js
+++ bot.js
@@ -831,8 +831,33 @@ async function dbSet(msgid, highbid, highbidder, reserve, updatemsg){//, auction
 	return dbmsg.edit(dbstr);
 }
 
-async function lots_of_messages_getter(channel, limit) {
-    const sum_messages = [];
+async function getMessages(channel: TextChannel, limit: number = 100): Promise<Message[]> {
+	let out: Message[] = []
+	if (limit <= 100) {
+	  let messages: Collection < string, Message > = await channel.messages.fetch({ limit: limit })
+	  out.push(...messages.array())
+	} else {
+	  let rounds = (limit / 100) + (limit % 100 ? 1 : 0)
+	  let last_id: string = ""
+	  for (let x = 0; x < rounds; x++) {
+		const options: ChannelLogsQueryOptions = {
+		  limit: 100
+		}
+		if (last_id.length > 0) {
+		  options.before = last_id
+		}
+		const messages: Collection < string, Message > = await channel.messages.fetch(options)
+		out.push(...messages.array())
+		last_id = messages.array()[(messages.array().length - 1)].id
+	  }
+	}
+console.log(out.length);
+	if(out.length > queuelimit){
+		out.length = queuelimit;
+	}
+  }
+  
+ /*  const sum_messages = [];
     let last_id;
 
     while (true) {
@@ -855,7 +880,7 @@ async function lots_of_messages_getter(channel, limit) {
 	}
 
 	return sum_messages;
-}
+} */
 
 
 
@@ -878,7 +903,7 @@ async function queuemsgcheck(){
 	
 	const allFindNexts = [];
 	//process all fetched messages
-	var messages = await lots_of_messages_getter(queuechannel, 500); //qchannel.messages.fetch();
+	var messages = await getMessages(queuechannel, 500); //qchannel.messages.fetch();
 	messages.forEach(async (msg) => {
 			// if qmsg isn't NO QUEUE, remove any found message ids while processing
 			// this will result in helping find any ids that are in qmsg that don't exist
